a2d81f36fdadb672300833e24b19c8eebaf278db
Christian Bodart <chris@bodar.com>
2012-07-30 11:45:32 +0100

    Adding type constants to Robot

--- src/robot.js
+++ src/robot.js
@@ -3,9 +3,26 @@ function Robot(x,y,orientation) {
 	this.coordinate = new GridCoordinate(x,y);
 	this.orientation = orientation;
 }
+//constants
+Robot.prototype.NORTH = new String("N");
+Robot.prototype.EAST = new String("E");
+Robot.prototype.SOUTH = new String("S");
+Robot.prototype.WEST = new String("W");
+
+//remember left and right are simply orientation changes not movement
+//should probably be named CLOCKWISE and ANTICLOCKWISE ;-) see Issue #4
+Robot.prototype.LEFT = new String("L");
+Robot.prototype.RIGHT = new String("R");
+
+Robot.prototype.FORWARD = new String("F");
+
+Robot.prototype.LOST = new String("LOST");
+
 //properties
 Robot.prototype.coordinate = void (GridCoordinate) || null;
 Robot.prototype.orientation = "";
+
+
 
 
 //methods
