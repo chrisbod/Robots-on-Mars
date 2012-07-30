77af2ddb05dcdbb777c8a8d8b424d9b40c4fbcff
Christian Bodart <chris@bodar.com>
2012-07-30 10:43:13 +0100

    stubbing out Robot type

---
+++ src/robot.js
@@ -0,0 +1,20 @@
+//uses GridCoordinate(grid.js)
+function Robot(x,y,orientation) {
+	this.coordinate = new GridCoordinate(x,y);
+	this.orientation = orientation;
+}
+//properties
+Robot.prototype.coordinate = void (GridCoordinate) || null;
+Robot.prototype.orientation = "";
+
+
+//methods
+Robot.prototype.turnLeft = function robot_turnLeft() {
+
+}
+Robot.prototype.turnRight = function robot_turnRight() {
+
+]
+Robot.prototype.moveForward = function robot_moveForward() {
+
+}
