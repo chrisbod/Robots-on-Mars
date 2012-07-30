fd2793496d96e2172ec393395bb51b9be5701185
Christian Bodart <chris@bodar.com>
2012-07-30 10:31:43 +0100

    initial thoughts on positioning system

--- src/grid.js
+++ src/grid.js
@@ -1,6 +1,17 @@
+//rushing headlong in again
 function RemoteGrid(width,height) {
 	this.width = width;//lower right
 	this.height = height;//upper right
-	//be lazy and store items on points of grid as x_y?
+	//be lazy and store positions on points of grid as x_y?
 	this.lookup = {};
+	//do we pre-populate the grid with positions - I think so;
 }
+function GridCoordinate(x,y) {
+	this.x = x;
+	this.y = y;
+}
+function GridPosition(coordinate) {
+	this.coordinate = coordinate;
+	this.items = []; //okay so according to the document it looks like a position will only have one item at a time (Robot or Scent);
+}
+
